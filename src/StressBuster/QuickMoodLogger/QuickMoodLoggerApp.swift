cd939ef8898086db2337d1c8e46ca9165f001237
PrimeCoder <primecoder.authors@gmail.com>
2025-02-04 13:48:58 +1100

    Fixed signing bundle id and added AppClip target

---
+++ src/StressBuster/QuickMoodLogger/QuickMoodLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickMoodLoggerApp.swift
+//  QuickMoodLogger
+//
+//  Created by ace on 4/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuickMoodLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LogEntryView()
+        }
+    }
+}
